c3804bc1eedd9d78792b6745a9310b712d1f81ac
drazzilblol <paskalj10@gmail.com>
2016-07-22 20:40:39 +0300

    another init

--- app/comix-list/comix-list.component.js
+++ app/comix-list/comix-list.component.js
@@ -4,7 +4,7 @@ angular.module('comixList').component('comixList', {
     templateUrl: 'comix-list/comix-list.template.html',
     controller: ['$http', function ComixListController($http) {
         var self = this;
-        $http.get('phones.json').then(function (res) {
+        $http.get('http://localhost:3000/comix').then(function (res) {
 
             self.comixes = res.data;
             console.log(res.data)
