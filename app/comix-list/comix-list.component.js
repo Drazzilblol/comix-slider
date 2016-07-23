8c27dd75f0426984ee7c8436966e2c67533421e0
drazzilblol <paskalj10@gmail.com>
2016-07-23 20:00:58 +0300

    merge server and client

--- app/comix-list/comix-list.component.js
+++ app/comix-list/comix-list.component.js
@@ -2,14 +2,20 @@
 
 angular.module('comixList').component('comixList', {
     templateUrl: 'comix-list/comix-list.template.html',
-    controller: ['$http', function ComixListController($http) {
+    controller: ['$http', '$scope', function ComixListController($http, $scope) {
         var self = this;
         $http.get('http://localhost:3000/comix').then(function (res) {
-
-            self.comixes = res.data;
+            $scope.comixes = res.data;
             console.log(res.data)
-
         });
+
+        $scope.addComix = function(comixUrl) {
+            $http.get('http://localhost:3000/comix/' + comixUrl).then(function (res) {
+                $scope.comixes.push(res.data);
+                console.log(res.data)
+            });
+
+        };
 
     }]
 });
